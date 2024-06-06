55277ed3d50f50043cd788a766323c1fb6e38d62
Ana-Maria <95433627+Ana-Maria-C@users.noreply.github.com>
2024-06-07 00:49:12 +0300

    +camera open

--- scancuisine/src/components/Navbar/Navbar.tsx
+++ scancuisine/src/components/Navbar/Navbar.tsx
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from "react";
-import { Menu, Button, Input, message } from "antd";
+import { Menu, Button, Input, message, Modal } from "antd";
 import { Link } from "react-router-dom";
 import {
   UserOutlined,
   SearchOutlined,
   CameraOutlined,
+  ChromeOutlined,
 } from "@ant-design/icons";
 import "./Navbar.css";
 import { useNavigate } from "react-router-dom";
@@ -33,6 +34,7 @@ function Navbar() {
 
   const [categories, setCategories] = useState<Category[]>([]);
   const [cuisines, setCuisines] = useState<Cuisine[]>([]);
+  const [cameraVisible, setCameraVisible] = useState(false);
 
   const navigate = useNavigate();
 
@@ -114,78 +116,125 @@ function Navbar() {
     navigate(`/cuisine/${cuisine.name}`);
   };
 
+  const openCamera = () => {
+    setCameraVisible(true);
+  };
+
+  const closeCamera = () => {
+    setCameraVisible(false);
+    let video = document.querySelector("video");
+    if (video && video.srcObject) {
+      let stream = video.srcObject as MediaStream;
+      let tracks = stream.getTracks();
+      tracks.forEach((track) => track.stop());
+    }
+  };
+
+  useEffect(() => {
+    if (cameraVisible) {
+      navigator.mediaDevices
+        .getUserMedia({ video: true })
+        .then((stream) => {
+          let video = document.querySelector("video");
+          if (video) {
+            video.srcObject = stream;
+            video.play();
+          }
+        })
+        .catch((error) => {
+          console.error("Error accessing camera:", error);
+          message.error("Failed to access camera. Please try again.");
+        });
+    }
+  }, [cameraVisible]);
+
   return (
-    <Menu
-      mode="horizontal"
-      className={`navbar ${visible || isPopupOpen ? "" : "hidden"}`}
-    >
-      <Menu.Item key="home">
-        <Link to="/home">Scan Cuisine</Link>
-      </Menu.Item>
-      <Menu.Item key="search" className="search-input">
-        <Input
-          prefix={<SearchOutlined />}
-          placeholder="Find a recipe"
-          onPressEnter={handleSearchEnter}
-          value={searchValue}
-          onChange={(e) => setSearchValue(e.target.value)}
-        />
-      </Menu.Item>
-      <SubMenu
-        key="categories"
-        title={<span className="categories-submenu">Categories</span>}
-        popupClassName="categories-popup"
+    <>
+      <Menu
+        mode="horizontal"
+        className={`navbar ${visible || isPopupOpen ? "" : "hidden"}`}
       >
-        {categories.map((category) => (
-          <Menu.Item
-            key={category.id}
-            onClick={() => handleMenuClickCategory(category)}
-            onMouseEnter={() => setIsPopupOpen(true)}
-            onMouseLeave={() => setIsPopupOpen(false)}
-          >
-            {category.name}
-          </Menu.Item>
-        ))}
-      </SubMenu>
-      <SubMenu
-        key="cuisines"
-        title={<span className="categories-submenu">Cuisines</span>}
-        popupClassName="cuisines-popup"
-      >
-        {cuisines.map((cuisine) => (
-          <Menu.Item
-            key={cuisine.id}
-            onClick={() => handleMenuClickCuisine(cuisine)}
-            onMouseEnter={() => setIsPopupOpen(true)}
-            onMouseLeave={() => setIsPopupOpen(false)}
-          >
-            {cuisine.name}
-          </Menu.Item>
-        ))}
-      </SubMenu>
-      <Menu.Item key="scan" className="camera-scan">
-        <CameraOutlined />
-      </Menu.Item>
-      {isLoggedIn ? (
-        <>
-          <Menu.Item key="logout" className="logout" onClick={handleLogout}>
-            <Link to="">Log Out</Link>
-          </Menu.Item>
-          <Menu.Item key="profile" className="myprofile">
-            <Link to="/myprofile">
-              <Button icon={<UserOutlined />} key="My Profile"></Button>
-            </Link>
-          </Menu.Item>
-        </>
-      ) : (
-        <Menu.Item key="login" className="logout">
-          <Link to="/login">Log In</Link>
+        <Menu.Item key="home">
+          <Link to="/home">Scan Cuisine</Link>
         </Menu.Item>
-      )}
-      <Menu.Item key="about">
-        <Link to="/about">About Us</Link>
-      </Menu.Item>
-    </Menu>
+        <Menu.Item key="search" className="search-input">
+          <Input
+            prefix={<SearchOutlined />}
+            placeholder="Find a recipe"
+            onPressEnter={handleSearchEnter}
+            value={searchValue}
+            onChange={(e) => setSearchValue(e.target.value)}
+          />
+        </Menu.Item>
+        <SubMenu
+          key="categories"
+          title={<span className="categories-submenu">Categories</span>}
+          popupClassName="categories-popup"
+        >
+          {categories.map((category) => (
+            <Menu.Item
+              key={category.id}
+              onClick={() => handleMenuClickCategory(category)}
+              onMouseEnter={() => setIsPopupOpen(true)}
+              onMouseLeave={() => setIsPopupOpen(false)}
+            >
+              {category.name}
+            </Menu.Item>
+          ))}
+        </SubMenu>
+        <SubMenu
+          key="cuisines"
+          title={<span className="categories-submenu">Cuisines</span>}
+          popupClassName="cuisines-popup"
+        >
+          {cuisines.map((cuisine) => (
+            <Menu.Item
+              key={cuisine.id}
+              onClick={() => handleMenuClickCuisine(cuisine)}
+              onMouseEnter={() => setIsPopupOpen(true)}
+              onMouseLeave={() => setIsPopupOpen(false)}
+            >
+              {cuisine.name}
+            </Menu.Item>
+          ))}
+        </SubMenu>
+        <Menu.Item key="scan" className="camera-scan" onClick={openCamera}>
+          <CameraOutlined />
+        </Menu.Item>
+        {isLoggedIn ? (
+          <>
+            <Menu.Item key="logout" className="logout" onClick={handleLogout}>
+              <Link to="">Log Out</Link>
+            </Menu.Item>
+            <Menu.Item key="profile" className="myprofile">
+              <Link to="/myprofile">
+                <Button icon={<UserOutlined />} key="My Profile"></Button>
+              </Link>
+            </Menu.Item>
+          </>
+        ) : (
+          <Menu.Item key="login" className="logout">
+            <Link to="/login">Log In</Link>
+          </Menu.Item>
+        )}
+        <Menu.Item key="about">
+          <Link to="/about">About Us</Link>
+        </Menu.Item>
+      </Menu>
+      <Modal
+        className="camera-modal"
+        title="Scan your fridge..."
+        visible={cameraVisible}
+        onCancel={closeCamera}
+        footer={null}
+      >
+        <video className="camera-modal-video"></video>
+        <div className="camera-modal-footer">
+          <button className="camera-modal-button"></button>
+          <button className="camera-modal-action-button"></button>
+        </div>
+      </Modal>
+    </>
   );
 }
 
